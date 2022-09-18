a7ff3bc51ec0ca38193a9533068ea551152dc914
FizzGemge <108047082+FizzGemge@users.noreply.github.com>
2022-09-18 15:20:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['6288296172396','6288296172396','6288296172396']
+global.premium = ['6288296172396']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
